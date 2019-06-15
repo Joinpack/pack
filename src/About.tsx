16f7990907badaa75625c9d4a60bdf83b60e3cba
Kevin Lee <kevinlee963@gmail.com>
2019-06-14 21:15:53 -0700

    Add skeleton routing and class components

---
+++ src/About.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+export interface AboutProps { }
+
+export class About extends React.Component<AboutProps, {}> {
+  render() {
+    return (
+      <div>About Us</div>
+    );
+  }
+};
+
+export default About;
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Home from './Home';
+import About from './About';
+
+export interface AppProps { }
+
+export class App extends React.Component<AppProps, {}> {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+      </Switch>
+    );
+  }
 }
 
 export default App;
