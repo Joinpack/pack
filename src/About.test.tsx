1f2682717c96737315db02b446cb431955e08f30
Kevin Lee <kevinlee963@gmail.com>
2019-06-14 22:29:34 -0700

    Code coverage (#1)
    * Enable source map
    
    * Configure codecov
    
    * Fix codecov call
    
    * Move codecov from global yarn
    
    * Add test for About page
    
    * Implement more configurations for code coverage

---
+++ src/About.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import 'jest-dom/extend-expect'
+
+import About from './About';
+
+it('renders about text', () => {
+  const { getByText } = render(<About />);
+  expect(getByText('About Us')).toBeInTheDocument();
+});
